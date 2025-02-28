2331b0d6026d117029078807718f6f27dc9c748a
Hanna Arefi Ardakani <darkshiira@hotmail.com>
2025-02-28 19:49:10 +0100

    added a lil message

--- cms/src/app/class/page.tsx
+++ cms/src/app/class/page.tsx
@@ -33,10 +33,12 @@ export default function ClassPage() {
     return (
         <div>
             Welcome to class page!!
-
+            {/* Im thinking you see all classes here, and then you have the option to edit/delete them */}
             {
                 classes===undefined?<div>Nej</div>:<div>Yay</div>
             }
+
+            Do you want to add a class? <a href="/class/create">Click here</a>
         </div>
     );
 }
