05d32078ce856062f73c95abdf149df1a6da4f6b
Hanna Arefi Ardakani <darkshiira@hotmail.com>
2025-03-05 12:12:34 +0100

    Added Delete path to route.

--- cms/src/app/api/class/[name]/route.ts
+++ cms/src/app/api/class/[name]/route.ts
@@ -29,3 +29,27 @@ export async function GET(req: NextRequest) : Promise<NextResponse> {
 
 
 }
+
+export async function DELETE(req: NextRequest) : Promise<NextResponse> {
+    const url = new URL(req.url);
+    const _id = url.pathname.split("/").pop();
+    try {
+        await dbConnect()
+        const correctClass = await Class.findOne({_id})
+        if (!correctClass) {
+            return NextResponse.json(null, { status: 503 });
+        } else {
+
+            const deleted = await correctClass.deleteOne({_id})
+
+        
+            return NextResponse.json(deleted, { status: 200 });
+        }
+
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(null, { status: 500 });
+    }
+
+
+}
