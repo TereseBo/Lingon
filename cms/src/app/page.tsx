3fe9a47f8ebaafd71ee02343bc86851a15bc9e39
TereseBo <terese.boderus@gmail.com>
2025-05-09 18:16:37 +0200

    Removed old code

--- cms/src/app/page.tsx
+++ cms/src/app/page.tsx
@@ -1,38 +1,12 @@
 "use client"
-import { useState } from "react";
-import { Employee } from "@/database/models/Employee"
+
 import {Menu} from "@/components/menu"
 
 export default function Home() {
-    const [petResult, setPetResult] = useState<Employee[] | null>(null);
-    async function tryDatabase() {
-        try {
-            const apiRes = await fetch(`/api`, {
-                method: "GET",
-            });
-
-            if (apiRes.ok) {
-                const data = await apiRes.json(); // Parse JSON data */
-
-                setPetResult(data); // Update state with parsed data
-            }
-        } catch (error) {
-            //TODO: Handle error
-            console.log(error);
-        }
-    }
-
+   
     return (
         <div>
             <Menu/>
-            <button onClick={tryDatabase}>Click me for fetch</button>
-            {petResult ? (
-                petResult.map((employee, index) => (
-                    <div key={index}>{employee.firstName || "Unknown Name"}</div>
-                ))
-            ) : (
-                <div>Loading...</div>
-            )}
         </div>
     );
 }
